0630ad2462bacea191bcf1256863a521d44afd21
m_akdogan@anadolu.edu.tr <38a909a9>
2017-12-14 15:58:15 +0300

    Default sayfası yazar listeleme kısmı

--- CengP/YazarListele.aspx.cs
+++ CengP/YazarListele.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,6 +12,38 @@ public partial class YazarListele : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            Yazar();
+        }
+    }
+    protected void Yazar()
+    {
+        DataTable dt = new DataTable();
+        string strQuery = "select * from members WHERE totalpoint>=2500 and onay=1 ORDER BY totalpoint DESC";
+        SqlCommand cmd = new SqlCommand(strQuery);
+        SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["CengP"].ConnectionString);
+        SqlDataAdapter sda = new SqlDataAdapter();
+        cmd.CommandType = CommandType.Text;
+        cmd.Connection = con;
+        try
+        {
+            con.Open();
+            sda.SelectCommand = cmd;
+            sda.Fill(dt);
+            ListView2.DataSource = dt;
+            ListView2.DataBind();
 
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+            sda.Dispose();
+            con.Dispose();
+        }
     }
 }
