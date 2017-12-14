c8685b957156304ce1c1e189d3014cc20647a921
SERHAT\SerhatGunes <serhat.gunes.92@gmail.com>
2017-12-14 18:01:34 +0300

    login düzeltildi

--- CengP/Admin/Login.aspx.cs
+++ CengP/Admin/Login.aspx.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.Configuration;
 
 public partial class Admin_Login : System.Web.UI.Page
 {
