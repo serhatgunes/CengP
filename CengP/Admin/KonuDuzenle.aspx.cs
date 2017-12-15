c7449125ba121efadda4542dcddf6a1671de0099
SERHAT\SerhatGunes <serhat.gunes.92@gmail.com>
2017-12-15 15:30:14 +0300

    konu düzenleme sayfası gridview ayarları

--- CengP/Admin/KonuDuzenle.aspx.cs
+++ CengP/Admin/KonuDuzenle.aspx.cs
@@ -11,4 +11,68 @@ public partial class Admin_KonuDuzenle : System.Web.UI.Page
     {
 
     }
+    protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.Header)
+        {
+
+            e.Row.Cells[1].ForeColor = System.Drawing.Color.White;
+            e.Row.Cells[2].ForeColor = System.Drawing.Color.White;
+
+            e.Row.Cells[1].Text = "ID";
+            e.Row.Cells[2].Text = "Konu Adı";
+
+            e.Row.Cells[0].Width = 170;
+            e.Row.Cells[1].Width = 60;
+            e.Row.Cells[2].Width = 350;
+        }
+    }
+    protected void GridView2_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.Header)
+        {
+
+            e.Row.Cells[1].ForeColor = System.Drawing.Color.White;
+            e.Row.Cells[2].ForeColor = System.Drawing.Color.White;
+
+            e.Row.Cells[1].Text = "ID";
+            e.Row.Cells[2].Text = "Konu Adı";
+
+            e.Row.Cells[0].Width = 170;
+            e.Row.Cells[1].Width = 60;
+            e.Row.Cells[2].Width = 350;
+        }
+    }
+    protected void GridView3_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.Header)
+        {
+
+            e.Row.Cells[1].ForeColor = System.Drawing.Color.White;
+            e.Row.Cells[2].ForeColor = System.Drawing.Color.White;
+
+            e.Row.Cells[1].Text = "ID";
+            e.Row.Cells[2].Text = "Konu Adı";
+
+            e.Row.Cells[0].Width = 170;
+            e.Row.Cells[1].Width = 60;
+            e.Row.Cells[2].Width = 350;
+        }
+    }
+    protected void GridView4_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.Header)
+        {
+
+            e.Row.Cells[1].ForeColor = System.Drawing.Color.White;
+            e.Row.Cells[2].ForeColor = System.Drawing.Color.White;
+
+            e.Row.Cells[1].Text = "ID";
+            e.Row.Cells[2].Text = "Konu Adı";
+
+            e.Row.Cells[0].Width = 170;
+            e.Row.Cells[1].Width = 60;
+            e.Row.Cells[2].Width = 350;
+        }
+    }
 }
