e7d83421d67ee4d552252cfa42830e3a9fab2f0d
SERHAT\SerhatGunes <serhat.gunes.92@gmail.com>
2017-12-15 15:39:17 +0300

    admin master page linkler düzeltildi

--- CengP/Admin/OnayliKullanici.aspx.cs
+++ CengP/Admin/OnayliKullanici.aspx.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 using System.Web.Configuration;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Admin_OnayliKullanici : System.Web.UI.Page
